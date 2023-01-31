7fbb4d023e9a2580256ad833261cbc873beff323
Wilian-N-Silva <wilian.silva209@gmail.com>
2023-01-31 09:40:49 -0300

    feat: Desafio completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 /*
 * CHALLENGE progresso do formulário
 
@@ -36,45 +38,139 @@ do formulário e zerar a barra de progresso novamente.
 */
 
 function App() {
-  return (
-    <div className='App'>
-      <h3>desafio fernandev</h3>
-      <h1>progresso do formulário</h1>
+	const [progressPercentage, setProgressPercentage] = useState(0);
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		maritalStatus: '',
+		gender: '',
+	});
 
-      <main>
-        {/* crie a barra de progresso aqui */}
-        <div className='form-group'>
-          <label htmlFor=''>Nome Completo</label>
-          <input />
-        </div>
-        <div className='form-group'>
-          <label htmlFor=''>E-mail</label>
-          <input />
-        </div>
-        <div className='form-group'>
-          <label htmlFor=''>Estado Civil</label>
-          <select>
-            <option value=''>- selecione...</option>
-            <option value='solteiro'>Solteiro</option>
-            <option value='casado'>Casado</option>
-            <option value='divorciado'>Divorciado</option>
-          </select>
-        </div>
-        <div className='form-group'>
-          <label htmlFor=''>Gênero</label>
-          <div className='radios-container'>
-            <span>
-              <input type='radio' /> Masculino
-            </span>
-            <span>
-              <input type='radio' /> Feminino
-            </span>
-          </div>
-        </div>
-        <button>Enviar Formulário</button>
-      </main>
-    </div>
-  );
+	const handleChanges = (event) => {
+		const { name, value } = event.target;
+		setFormData((prevState) => {
+			const newFormData = { ...prevState, [name]: value };
+			return newFormData;
+		});
+	};
+
+	const validateForm = () => {
+		const numOfFields = Object.entries(formData).length;
+		let numOfValidFields = 0;
+
+		Object.entries(formData).forEach(([key, value]) => {
+			if (key === 'name') {
+				const splited = value.split(' ');
+				numOfValidFields += !!splited[1] && splited[1] !== ' ' ? 1 : 0;
+
+				return;
+			}
+
+			if (key === 'email') {
+				const exp =
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+				numOfValidFields += exp.test(value) ? 1 : 0;
+
+				return;
+			}
+
+			numOfValidFields += value !== '' ? 1 : 0;
+		});
+
+		const progressCalc = (numOfValidFields / numOfFields) * 100;
+
+		setProgressPercentage(progressCalc);
+	};
+
+	const handleSubmit = () => {
+		alert('Formulário preenchido com sucesso');
+		setFormData({
+			name: '',
+			email: '',
+			maritalStatus: '',
+			gender: '',
+		});
+	};
+
+	useEffect(() => {
+		validateForm();
+	}, [formData]);
+
+	return (
+		<div className="App">
+			<h3>desafio fernandev</h3>
+			<h1>progresso do formulário</h1>
+			<main>
+				{
+					<div className="bar-container">
+						<div className="bar" style={{ width: `${progressPercentage}%` }} />
+					</div>
+				}
+				<div className="form-group">
+					<label htmlFor="">Nome Completo</label>
+					<input
+						name={'name'}
+						value={formData['name']}
+						onChange={handleChanges}
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="">E-mail</label>
+					<input
+						name={'email'}
+						value={formData['email']}
+						onChange={handleChanges}
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="">Estado Civil</label>
+					<select
+						name={'maritalStatus'}
+						onChange={handleChanges}
+						value={formData['maritalStatus']}
+					>
+						<option value="">- selecione...</option>
+						<option value="solteiro">Solteiro</option>
+						<option value="casado">Casado</option>
+						<option value="divorciado">Divorciado</option>
+					</select>
+				</div>
+				<div className="form-group">
+					<label htmlFor="">Gênero</label>
+					<div className="radios-container">
+						<span>
+							<input
+								name={'gender'}
+								type="radio"
+								value={'m'}
+								checked={
+									formData['gender'] !== '' && formData['gender'] === 'm'
+								}
+								onChange={handleChanges}
+							/>{' '}
+							Masculino
+						</span>
+						<span>
+							<input
+								name={'gender'}
+								type="radio"
+								value={'f'}
+								checked={
+									formData['gender'] !== '' && formData['gender'] === 'f'
+								}
+								onChange={handleChanges}
+							/>{' '}
+							Feminino
+						</span>
+					</div>
+				</div>
+				<button onClick={handleSubmit} disabled={progressPercentage !== 100}>
+					Enviar Formulário
+				</button>
+			</main>
+		</div>
+	);
 }
 
 export default App;
